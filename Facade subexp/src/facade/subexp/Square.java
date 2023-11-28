5fe97cf2a43424d38abd9cced61d37e7b873ec9c
Osama-455 <151600764+Osama-455@users.noreply.github.com>
2023-11-28 10:36:05 +0500

    Add files via upload

---
+++ Facade subexp/src/facade/subexp/Square.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facade.subexp;
+
+/**
+ *
+ * @author fa20-bse-149
+ */
+public class Square implements Shape {
+
+    @Override
+    public void draw() {
+       
+      System.out.println("Square::draw()");
+    }
+    
+}
